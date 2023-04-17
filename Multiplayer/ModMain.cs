b36dd97de4ff1cf11b4433d37376d39b3f40143e
DeltaNeverUsed <sinus06@hotmail.com>
2023-04-17 13:07:29 +0200

    Initial commit

---
+++ Multiplayer/ModMain.cs
@@ -0,0 +1,15 @@
+﻿using MelonLoader;
+using UnityEngine;
+
+namespace MultiplayerMod
+{
+    public class ModMain : MelonMod
+    {
+        public override void OnSceneWasLoaded(int buildIndex, string sceneName)
+        {
+            if (buildIndex != 1)
+                return;
+            
+        }
+    }
+}
